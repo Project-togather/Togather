029d8fd2e5f6e3e9d41fe31c4cf6beca5d9d9dcf
(jh) <(whwnsgk94@naver.com)>
2023-10-16 16:13:30 +0900

    enroll
    회원가입

--- togatherProject/src/main/java/com/kh/spring/member/model/dao/MemberDao.java
+++ togatherProject/src/main/java/com/kh/spring/member/model/dao/MemberDao.java
@@ -3,6 +3,7 @@ package com.kh.spring.member.model.dao;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.kh.spring.attachment.model.vo.Attachment;
 import com.kh.spring.member.model.vo.Member;
 
 @Repository
@@ -14,5 +15,22 @@ public class MemberDao {
 	
 	public int idCheck(SqlSessionTemplate sqlSession , String checkId) {
 		return sqlSession.selectOne("memberMapper.idCheck", checkId);
+	}
+	
+	public int insertMember(SqlSessionTemplate sqlSession , Member m) {
+		
+		return sqlSession.insert("memberMapper.insertMember", m);
+		
+	}
+	
+	public String bringMemNo(SqlSessionTemplate sqlSession , Member m){
+
+		String MemNo =  sqlSession.selectOne("memberMapper.bringMemNo",m);
+		return MemNo;
+		
+	}
+	
+	public int insertProfileImage(SqlSessionTemplate sqlSession , Attachment at) {
+		return sqlSession.insert("memberMapper.insertProfileImage", at);
 	}
 }
--- togatherProject/src/main/java/com/kh/spring/member/model/service/MemberServiceImpl.java
+++ togatherProject/src/main/java/com/kh/spring/member/model/service/MemberServiceImpl.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kh.spring.attachment.model.vo.Attachment;
 import com.kh.spring.member.model.dao.MemberDao;
 import com.kh.spring.member.model.vo.Member;
 
@@ -31,9 +32,28 @@ public class MemberServiceImpl implements MemberService {
 	}
 
 	@Override
-	public int inserMember(Member m) {
-		
-		return 0;
+	public int insertMember(Member m) {
+		int result = mDao.insertMember(sqlSession, m);
+		return result;
+	}
+
+	
+	/**
+	 *회원번호 가져오기
+	 */
+	@Override
+	public String bringMemNo(Member m) {
+		String MemNo = mDao.bringMemNo(sqlSession, m);
+		return MemNo;
+	}
+
+	/**
+	 *프사넣기
+	 */
+	@Override
+	public int insertProfileImage(Attachment at) {
+		int result = mDao.insertProfileImage(sqlSession , at);
+		return result;
 	}
 	
 }
