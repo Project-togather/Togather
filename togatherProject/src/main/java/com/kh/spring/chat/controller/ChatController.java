89e6ef2da3c4bafe7bda303124cb8b7fdcaffa18
조준하 <48859949+whwnsgk@users.noreply.github.com>
2023-10-30 20:42:53 +0900

    nothing
    암것도 안바뀜

--- togatherProject/src/main/java/com/kh/spring/chat/controller/ChatController.java
+++ togatherProject/src/main/java/com/kh/spring/chat/controller/ChatController.java
@@ -22,12 +22,15 @@ public class ChatController {
 	//채팅창 입장
 	@RequestMapping(value = "chat.ch")
 	public String moveChatForm(HttpSession session ,  String targetNo) {
+		System.out.println("시발 뭐지");
 		//먼저 방이 있나없나 봐야함 
 		//채팅방이 없으면 채팅방 테이블에 추가를 해줘야함
 		//있으면 여기 단계 생략하고 바로 불러오기 해야겠지
 		//채팅방이 있는지 없는지 검사는 채팅보내는 사람 의 채팅방번호 select + 채팅받는사람의 채팅방번호 select 한값이 2가 되어야함
 		String memNo = ((Member)session.getAttribute("loginMember")).getMemNo();
-		System.out.println(memNo);
+		System.out.println("시발 뭐지2");
+		System.out.println("너냐? +"  memNo);
+		System.out.println("시발 뭐지3");
 		System.out.println(targetNo);
 		//hash쓰기 귀찮은데 걍 두번 왔다갔다 해서 결과값곱해서 1인걸로 하자
  		
@@ -36,17 +39,24 @@ public class ChatController {
 		//탐색을 잘해봐서 곂치는 방번호가 있나봐야함
 		
 		
-//		for (int i = 0; i < cr1.size() ; i ++) {
-//			System.out.println(cr1.get(i));
-//		}
-//		
-//		for (int i = 0; i < cr2.size() ; i ++) {
-//			System.out.println(cr2.get(i));
-//		}
+		for (int i = 0; i < cr1.size() ; i ++) {
+			System.out.println(cr1.get(i));
+		}
 		
-		
-		
-		
+		for (int i = 0; i < cr2.size() ; i ++) {
+			System.out.println(cr2.get(i));
+		}
+		int count = 0 ; 
+		System.out.println("넌 나와야지");
+		for(int i = 0 ; i < cr1.size(); i++) {
+			for(int j = 0 ; i < cr2.size() ; j ++) {
+				if(cr1.get(i).getChatRoomNo().equals(cr2.get(j).getChatRoomNo())) {
+					System.out.println("곂치는거 : " + cr1.get(i).getChatRoomNo()  + cr2.get(j).getChatRoomNo() );
+					count ++ ; 
+				}
+			}
+		}
+		System.out.println(count);
 		
 		
 		//채팅방 불러오기
