fb9110bfe9215b2d6691122c6f201d2d830bbfbb
(김상호) <(shkim3657@nate.com)>
2023-11-02 17:50:26 +0900

    신고기능

--- togatherProject/src/main/java/com/kh/spring/admin/member/model/dao/AdminDao.java
+++ togatherProject/src/main/java/com/kh/spring/admin/member/model/dao/AdminDao.java
@@ -9,67 +9,84 @@ import org.springframework.stereotype.Repository;
 import com.kh.spring.admin.member.model.vo.BlackList;
 import com.kh.spring.common.model.vo.PageInfo;
 import com.kh.spring.member.model.vo.Member;
+import com.kh.spring.report.model.vo.Report;
 
 @Repository
 public class AdminDao {
-	
-	//-----회원 관리------
+
+	// -----회원 관리------
 	public int selectMemberListCount(SqlSession sqlsession) {
 		return sqlsession.selectOne("admemberMapper.selectMemberCount");
 	}
-	
-	public ArrayList<Member> selectMemberList1(SqlSession sqlsession,PageInfo pi){
-		
-		int offset = (pi.getCurrentPage() -1)*pi.getBoardLimit();
+
+	public ArrayList<Member> selectMemberList1(SqlSession sqlsession, PageInfo pi) {
+
+		int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit();
 		int limit = pi.getBoardLimit();
-		
+
 		RowBounds rowBounds = new RowBounds(offset, limit);
-		return (ArrayList)sqlsession.selectList("admemberMapper.selectMemberList",null, rowBounds);
+		return (ArrayList) sqlsession.selectList("admemberMapper.selectMemberList", null, rowBounds);
 	}
-	
-	public ArrayList<Member> selectMemberList2(SqlSession sqlsession){
-		
-		
-		return (ArrayList)sqlsession.selectList("admemberMapper.selectMemberList2");
+
+	public ArrayList<Member> selectMemberList2(SqlSession sqlsession) {
+
+		return (ArrayList) sqlsession.selectList("admemberMapper.selectMemberList2");
 	}
-	
-	
-	public Member selectMemberList3 (SqlSession sqlsession,String memNo){
-		return  sqlsession.selectOne("admemberMapper.selectMemberList3",memNo);
+	//신고조회
+	public ArrayList<Report> selectReport(SqlSession sqlsession ,PageInfo pi) {
+
+		return (ArrayList) sqlsession.selectList("admemberMapper.selectReport");
 	}
-	
-	//-----블랙리스트 관리------
-	//insert시 id 체크
-	public int idCheck(SqlSession sqlsession,String checkId) {
-		return sqlsession.selectOne("admemberMapper.idCheck",checkId);
+
+	public Member selectMemberList3(SqlSession sqlsession, String memNo) {
+		return sqlsession.selectOne("admemberMapper.selectMemberList3", memNo);
 	}
-	//id 체크 후 insert
-	public int insertblackList(SqlSession sqlsession,String userId) {
-		return sqlsession.insert("admemberMapper.insertblackList",userId);
+
+	// -----블랙리스트 관리------
+	// insert시 id 체크
+	public int idCheck(SqlSession sqlsession, String checkId) {
+		return sqlsession.selectOne("admemberMapper.idCheck", checkId);
 	}
-	//블랙리스트 리스트
+
+	// id 체크 후 insert
+	public int insertblackList(SqlSession sqlsession, String userId) {
+		return sqlsession.insert("admemberMapper.insertblackList", userId);
+	}
+
+	// 블랙리스트 리스트
 	public int selectBlackListCount(SqlSession sqlsession) {
 		return sqlsession.selectOne("admemberMapper.selectBlackListCount");
 	}
-	//블랙리스트 페이지바
-	public ArrayList<BlackList> selectBlackList(SqlSession sqlSession,PageInfo pi){
-		int offset = (pi.getCurrentPage() -1)*pi.getBoardLimit();
+
+	// 블랙리스트 페이지바
+	public ArrayList<BlackList> selectBlackList(SqlSession sqlSession, PageInfo pi) {
+		int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit();
 		int limit = pi.getBoardLimit();
-		
+
 		RowBounds rowBounds = new RowBounds(offset, limit);
-		return (ArrayList)sqlSession.selectList("admemberMapper.selectBlackList",null, rowBounds);
+		return (ArrayList) sqlSession.selectList("admemberMapper.selectBlackList", null, rowBounds);
 	}
-	//블랙리스트 삭제
-	public int updateBlackList(SqlSession sqlsession,String memId) {
-		return sqlsession.update("admemberMapper.updateBlackList",memId);
+
+	// 블랙리스트 삭제
+	public int updateBlackList(SqlSession sqlsession, String memId) {
+		return sqlsession.update("admemberMapper.updateBlackList", memId);
 	}
-	
-	//회원수정
-	public int adUpdateMember(SqlSession sqlsession,Member m) {
-		return sqlsession.update("admemberMapper.adUpdateMember",m);
+
+	// 회원수정
+	public int adUpdateMember(SqlSession sqlsession, Member m) {
+		return sqlsession.update("admemberMapper.adUpdateMember", m);
 	}
-	
-	public int insertExcel(SqlSession sqlsession,Member excel) {
-		return sqlsession.insert("admemberMapper.insertExcel",excel);
+
+	public int insertExcel(SqlSession sqlsession, Member excel) {
+		return sqlsession.insert("admemberMapper.insertExcel", excel);
+	}
+
+	public int insertReport(SqlSession sqlsession, Report r) {
+		return sqlsession.insert("admemberMapper.insertReport", r);
+	}
+
+	// 신고리스트조회
+	public int selectReportCount(SqlSession sqlsession) {
+		return sqlsession.selectOne("admemberMapper.selectReportCount");
 	}
 }
